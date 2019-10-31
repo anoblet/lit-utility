6cbbc358ad001b8fe1254ab8d5335673a87eef7f
Andrew Noblet <andrewbnoblet@gmail.com>
2019-10-31 19:01:51 -0400

    Chore: Save

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-export const propsFrom = o => ({
-  ...Object.entries(o).map(([k, v]) => [`.{k}`, v])
-});
-
-export const eventsFrom = o => ({
-  ...Object.entries(o).map(([k, v]) => [`@{k}`, v])
-});
+export { spreadProps } from "./spread-props";
---
+++ src/spread-props.ts
@@ -0,0 +1,17 @@
+import { directive } from "lit-html";
+
+const previousProps = new WeakMap();
+
+export const spreadProps = directive(props => part => {
+  const prev = previousProps.get(part);
+  if (prev === props) {
+    return;
+  }
+  previousProps.set(part, props);
+
+  Object.entries(props).forEach(([k, v]) => {
+    if (v !== part.committer.element[k]) {
+      part.committer.element[k] = v;
+    }
+  });
+});
